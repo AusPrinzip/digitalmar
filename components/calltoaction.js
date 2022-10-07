7735a6d59aca5c6f0d81bc588e288bf4108e0060
AusPrinzip <cdmdb@hotmail.com>
2022-10-07 08:53:32 -0600

    replaced contact message form with calltoaction

--- components/calltoaction.js
+++ components/calltoaction.js
@@ -12,7 +12,7 @@ class Calltoaction extends HTMLElement {
           <div class="col-lg-6 text-center">
             <h3>Don't be shy, ask for a non-binding quote!</h3>
             <p>We will gladly give you a quote for your needs in a timely manner. Do not hesitate!</p>
-            <a class="cta-btn" href="#">Call To Action</a>
+            <a class="cta-btn" href="https://calendly.com/digitalmar">Schedule a meeting</a>
           </div>
         </div>
 
